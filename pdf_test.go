7ce72546ceecb698a00bed3cb09724340fb39866
chenbiao <149027310@qq.com>
2023-11-17 17:55:11 +0800

    init

--- pdf_test.go
+++ pdf_test.go
@@ -4,12 +4,13 @@ import (
 	dgctx "github.com/darwinOrg/go-common/context"
 	dgimk "github.com/darwinOrg/go-imagick"
 	dglogger "github.com/darwinOrg/go-logger"
+	"os"
 	"testing"
 )
 
 func TestConvertPdfToImage(t *testing.T) {
 	ctx := &dgctx.DgContext{TraceId: "123"}
-	_, err := dgimk.ConvertPdfToImage(ctx, "test.pdf", 800, 1212, 200, 100, ".")
+	_, err := dgimk.ConvertPdfToImage(ctx, os.Getenv("pdfFile"), 800, 1212, 200, 100, ".")
 	if err != nil {
 		dglogger.Error(ctx, err)
 	}
