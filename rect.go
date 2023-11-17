7c98f9a95b50d0f2d96356232ae7088fc46f8fcc
chenbiao <149027310@qq.com>
2023-11-17 18:32:02 +0800

    init

--- rect.go
+++ rect.go
@@ -6,7 +6,7 @@ import (
 	"gopkg.in/gographics/imagick.v3/imagick"
 )
 
-func DrawHollowRectOnImage(ctx *dgctx.DgContext, imageFile string, outImageDir string, leftTopX, leftTopY, rightBottomX, rightBottomY float64, strokeWidth float64, color string) error {
+func DrawHollowRectOnImage(ctx *dgctx.DgContext, sourceImageFile string, destImageFile string, leftTopX, leftTopY, rightBottomX, rightBottomY float64, strokeWidth float64, color string) error {
 	imagick.Initialize()
 	defer imagick.Terminate()
 	mw := imagick.NewMagickWand()
@@ -16,13 +16,13 @@ func DrawHollowRectOnImage(ctx *dgctx.DgContext, imageFile string, outImageDir s
 	dw := imagick.NewDrawingWand()
 	defer dw.Destroy()
 
-	if err := mw.ReadImage(imageFile); err != nil {
-		dglogger.Errorf(ctx, "[file: %s] 文件读取失败", imageFile)
+	if err := mw.ReadImage(sourceImageFile); err != nil {
+		dglogger.Errorf(ctx, "[file: %s] 文件读取失败", sourceImageFile)
 		return err
 	}
 
 	if err := dw.PushDrawingWand(); err != nil {
-		dglogger.Errorf(ctx, "[file: %s] PushDrawingWand error: %v", imageFile, err)
+		dglogger.Errorf(ctx, "[file: %s] PushDrawingWand error: %v", sourceImageFile, err)
 		return err
 	}
 
@@ -37,17 +37,17 @@ func DrawHollowRectOnImage(ctx *dgctx.DgContext, imageFile string, outImageDir s
 	dw.Rectangle(leftTopX, leftTopY, rightBottomX, rightBottomY)
 
 	if err := dw.PopDrawingWand(); err != nil {
-		dglogger.Errorf(ctx, "[file: %s] PopDrawingWand error: %v", imageFile, err)
+		dglogger.Errorf(ctx, "[file: %s] PopDrawingWand error: %v", sourceImageFile, err)
 		return err
 	}
 
 	if err := mw.DrawImage(dw); err != nil {
-		dglogger.Errorf(ctx, "[file: %s] DrawImage error: %v", imageFile, err)
+		dglogger.Errorf(ctx, "[file: %s] DrawImage error: %v", sourceImageFile, err)
 		return err
 	}
 
-	if err := mw.WriteImage(outImageDir); err != nil {
-		dglogger.Errorf(ctx, "[file: %s] WriteImage error: %v", imageFile, err)
+	if err := mw.WriteImage(destImageFile); err != nil {
+		dglogger.Errorf(ctx, "[file: %s] WriteImage error: %v", sourceImageFile, err)
 		return err
 	}
 
