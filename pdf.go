ff39b4a5cb5ecf0624f60bc0fb3c80fb2846ac36
chenbiao <149027310@qq.com>
2023-11-18 09:39:18 +0800

    pdf

--- pdf.go
+++ pdf.go
@@ -5,13 +5,12 @@ import (
 	dglogger "github.com/darwinOrg/go-logger"
 	"github.com/google/uuid"
 	"gopkg.in/gographics/imagick.v3/imagick"
-	"strconv"
 )
 
 // ConvertPdfToImage 转换pdf为图片格式
 // @resolution: 扫描精度
 // @CompressionQuality: 图片质量: 1~100
-func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint, pageHeight uint, resolution float64, compressionQuality uint, outImageDir string) ([]string, error) {
+func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint, pageHeight uint, resolution float64, compressionQuality uint, outImageDir string) (string, error) {
 	imagick.Initialize()
 	defer imagick.Terminate()
 	mw := imagick.NewMagickWand()
@@ -19,12 +18,12 @@ func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint,
 
 	if err := mw.SetResolution(resolution, resolution); err != nil {
 		dglogger.Errorf(ctx, "[file: %s] 扫描精度[%f]设置失败", pdfFilePath, resolution)
-		return nil, err
+		return "", err
 	}
 
 	if err := mw.ReadImage(pdfFilePath); err != nil {
 		dglogger.Errorf(ctx, "[file: %s] 文件读取失败", pdfFilePath)
-		return nil, err
+		return "", err
 	}
 
 	var pages = int(mw.GetNumberImages())
@@ -32,8 +31,7 @@ func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint,
 
 	// 裁剪会使页数增加
 	addPages := 0
-	imageFilePrefix := outImageDir + "/" + uuid.NewString() + "-"
-	var imageFiles []string
+	var mws []*imagick.MagickWand
 
 	for i := 0; i < pages; i++ {
 		// This being the page offset
@@ -42,7 +40,7 @@ func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint,
 		// 压平图像，去掉alpha通道，防止JPG中的alpha变黑,用在ReadImage之后
 		if err := mw.SetImageAlphaChannel(imagick.ALPHA_CHANNEL_REMOVE); err != nil {
 			dglogger.Debugf(ctx, "[file: %s] 压平图像失败：%v", pdfFilePath, err)
-			return nil, err
+			return "", err
 		}
 
 		mw.SetImageFormat("jpg")
@@ -64,9 +62,7 @@ func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint,
 
 			// 左半页
 			mw.CropImage(pageWidth, pageHeight, 0, 0)
-			imageFilePath := imageFilePrefix + strconv.Itoa(i+addPages) + ".jpg"
-			mw.WriteImage(imageFilePath)
-			imageFiles = append(imageFiles, imageFilePath)
+			mws = append(mws, mw.GetImage())
 
 			// 右半页
 			leftMw.SetImageFormat("jpg")
@@ -74,19 +70,32 @@ func ConvertPdfToImage(ctx *dgctx.DgContext, pdfFilePath string, pageWidth uint,
 			leftMw.SetImageCompressionQuality(compressionQuality)
 			leftMw.CropImage(pageWidth, pageHeight, int(pageWidth), 0)
 			addPages++
-			imageFilePath = imageFilePrefix + strconv.Itoa(i+addPages) + ".jpg"
-			leftMw.WriteImage(imageFilePath)
-			imageFiles = append(imageFiles, imageFilePath)
-			leftMw.Destroy()
+			mws = append(mws, leftMw)
 		} else {
 			mw.ThumbnailImage(pageWidth, pageHeight)
-			imageFilePath := imageFilePrefix + strconv.Itoa(i+addPages) + ".jpg"
-			mw.WriteImage(imageFilePath)
-			imageFiles = append(imageFiles, imageFilePath)
+			mws = append(mws, mw.GetImage())
 		}
 	}
 
-	dglogger.Infof(ctx, "[file: %s] 转换完毕，共%d个图片文件", pdfFilePath, len(imageFiles))
+	defer func() {
+		for i := 0; i < len(mws); i++ {
+			mws[i].Destroy()
+		}
+	}()
 
-	return imageFiles, nil
+	fmw := mws[0]
+	for i := 1; i < len(mws); i++ {
+		fmw.AddImage(mws[i])
+	}
+	fmw.SetFirstIterator()
+	amw := fmw.AppendImages(true)
+	defer amw.Destroy()
+	outImageFile := outImageDir + "/" + uuid.NewString() + ".jpg"
+	if err := amw.WriteImage(outImageFile); err != nil {
+		dglogger.Debugf(ctx, "[file: %s] 导出图片文件失败：%v", pdfFilePath, err)
+		return "", err
+	}
+
+	dglogger.Infof(ctx, "[file: %s] 转换完毕，导出图片文件：%s", pdfFilePath, outImageFile)
+	return outImageFile, nil
 }
